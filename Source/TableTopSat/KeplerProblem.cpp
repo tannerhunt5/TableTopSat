55d2d4b8f10e596ad702c50bc529171edb1547d5
tannerhunt5 <tannerhunt5@tamu.edu>
2019-05-06 18:16:09 -0500

    Added KeplerProblem Actor

---
+++ Source/TableTopSat/KeplerProblem.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "KeplerProblem.h"
+#include <math.h>
+
+// Sets default values
+AKeplerProblem::AKeplerProblem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKeplerProblem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKeplerProblem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+float AKeplerProblem::FindZeta(FVector r0, FVector v0)
+{
+	return (pow(v0.Size(),2) / 2) - (mu / r0.Size());
+}
+
---
+++ Source/TableTopSat/KeplerProblem.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KeplerProblem.generated.h"
+
+UCLASS()
+class TABLETOPSAT_API AKeplerProblem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AKeplerProblem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Constants
+	float mu = 398600.4418;
+	float small = .000001;
+	float pi = 3.14159265358979; // pi to 15 digits
+
+	FVector r_ijk0;
+	FVector v_ijk0;
+
+	float Zeta;
+	float Alpha;
+
+	float FindZeta(FVector r0, FVector v0);
+};
